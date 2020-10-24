c7a3570868fbc1aaf3ee21bf2eb2c8f536bb8b9c
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-10-24 00:39:12 -0500

    create forms, send and recieve text, save to db

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from "react-hook-form";
+
 
 function App() {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data, r) => {
+    console.log(Number(data.phone))
+    fetch(`http://localhost:8000/messages`, {
+    // fetch(`https://guarded-taiga-97709.herokuapp.com/messages`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        content: data.content,
+        yourphone: "+15125185935",
+        myphone: `+${data.phone}`,
+        isoutgoing: true,
+      })
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data)
+      })
+    // r.target.reset();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="hello" className="HiContainer">
+      <div className="HiTitle">
+        <h1 style={{ color: "rgb(212, 175, 55)" }}>send a <br />text</h1>
+      </div>
+      <div className="HiForm">
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+
+          <textarea
+            name="content"
+            placeholder="your message"
+            id="comment"
+            ref={
+              register({
+                required: true
+              })
+            }
+          /><br />
+          {errors.content && "Please include a message"}<br />
+
+          <input
+            name="phone"
+            placeholder="recipient's phone number"
+            type="tel"
+            id="phone"
+            ref={
+              register({
+                required: "Please enter the phone number where you would like to send this message",
+                maxLength: {
+                  value: 20,
+                  message: "Please enter a valid phone number"
+                }
+              })
+            }
+          /><br />
+          {errors.phone && errors.phone.message}<br />
+
+          <button id="hiButton" type="submit" value="Submit" >send</button>
+        </form>
+      </div>
     </div>
   );
 }
