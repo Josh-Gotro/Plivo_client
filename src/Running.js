5bd78d1a3330d55ab28dfa252c8cbc836974d0ce
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-10-28 15:03:39 -0500

    action cable update

--- src/Running.js
+++ src/Running.js
@@ -6,7 +6,7 @@ const Running = (props) => {
         console.log("recent individual message", props.message)
         if(props.message.isoutgoing === true){
             return <p className="Red">From: {props.message.From} <br /> Message: {props.message.Text}<br /></p>
-        } else {
+        } else { 
             return <p className="Green">From: {props.message.From} <br /> Message: {props.message.Text}<br /></p>
         }
     }
